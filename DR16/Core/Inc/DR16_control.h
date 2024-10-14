99aa9162b99f1b257f3bd729e39ca1d182f9e333
liyuxu800 <1059031094@qq.com>
2024-10-14 20:46:21 +0800

    add DR16

---
+++ DR16/Core/Inc/DR16_control.h
@@ -0,0 +1,26 @@
+#ifndef DR16_CONTROL
+#define DR16_CONTROL
+
+/* ----------------------- RC Channel Definition---------------------------- */ 
+#define RC_CH_VALUE_MIN              ((uint16_t)364 ) 
+#define RC_CH_VALUE_OFFSET           ((uint16_t)1024) 
+#define RC_CH_VALUE_MAX              ((uint16_t)1684)
+/* ----------------------- RC Switch Definition----------------------------- */
+#define RC_SW_UP                     ((uint16_t)1) 
+#define RC_SW_MID                    ((uint16_t)3) 
+#define RC_SW_DOWN                   ((uint16_t)2)
+/* ----------------------- PC Key Definition-------------------------------- */
+#define KEY_PRESSED_OFFSET_W         ((uint16_t)0x01<<0) 
+#define KEY_PRESSED_OFFSET_S         ((uint16_t)0x01<<1) 
+#define KEY_PRESSED_OFFSET_A         ((uint16_t)0x01<<2) 
+#define KEY_PRESSED_OFFSET_D         ((uint16_t)0x01<<3) 
+#define KEY_PRESSED_OFFSET_Q         ((uint16_t)0x01<<4) 
+#define KEY_PRESSED_OFFSET_E         ((uint16_t)0x01<<5) 
+#define KEY_PRESSED_OFFSET_SHIFT     ((uint16_t)0x01<<6) 
+#define KEY_PRESSED_OFFSET_CTRL      ((uint16_t)0x01<<7) 
+#define  RC_FRAME_LENGTH                    18u     
+
+
+void RemoteDataProcess(uint8_t *pData);
+
+#endif
---
+++ DR16/Core/Src/DR16_control.c
@@ -0,0 +1,94 @@
+#include "stm32f4xx_hal.h"
+#include "DR16_control.h"
+
+/* ----------------------- Data Struct ------------------------------------- */
+typedef __packed struct
+{
+    struct
+    {
+        uint16_t ch0;
+        uint16_t ch1;
+        uint16_t ch2;
+        uint16_t ch3;
+        uint8_t s1;
+        uint8_t s2;
+    } rc;
+    struct
+    {
+        int16_t x;
+        int16_t y;
+        int16_t z;
+        uint8_t press_l;
+        uint8_t press_r;
+    } mouse;
+    struct
+    {
+        uint16_t v;
+    } key;
+} RC_Ctl_t;
+/* ----------------------- Internal Data ----------------------------------- */
+volatile unsigned char sbus_rx_buffer[2][RC_FRAME_LENGTH]; // double sbus rx buffer to save data
+static RC_Ctl_t RC_CtrlData;
+
+/* ----------------------- Function Implements  ---------------------------- */
+
+/******************************************************************************
+ * @fn      RC_Init
+ *
+ * @brief   configure stm32 usart2 port
+ *          -   USART Parameters
+ *              -   100Kbps
+ *              -   8-N-1
+ *          -   DMA Mode
+ *
+ * @return  None.
+ *
+ * @note    This code is fully tested on STM32F405RGT6 Platform, You can port
+it
+ *          to the other platform. Using doube buffer to receive data prevent
+losing data.
+ */
+
+// 此处官方实例代码为标准库的各个外设初始化，此处删除。
+
+/******************************************************************************
+ * @fn      RemoteDataProcess
+ *
+ * @brief   resolution rc protocol data.
+ * @pData   a point to rc receive buffer.
+ * @return  None.
+ * @note    RC_CtrlData is a global variable.you can deal with it in other place.
+ */
+void RemoteDataProcess(uint8_t *pData) // 用于解析数据，pData指向接收缓冲区的指针，存储了从遥控器接收到的数据
+{
+    if (pData == NULL) 
+    {
+        return;
+    }
+
+    RC_CtrlData.rc.ch0 = ((int16_t)pData[0] | ((int16_t)pData[1] << 8)) & 0x07FF;
+    RC_CtrlData.rc.ch1 = (((int16_t)pData[1] >> 3) | ((int16_t)pData[2] << 5)) & 0x07FF;
+    RC_CtrlData.rc.ch2 = (((int16_t)pData[2] >> 6) | ((int16_t)pData[3] << 2) |
+                          ((int16_t)pData[4] << 10)) &
+                         0x07FF;
+    RC_CtrlData.rc.ch3 = (((int16_t)pData[4] >> 1) | ((int16_t)pData[5] << 7)) &
+                         0x07FF;
+
+    RC_CtrlData.rc.s1 = ((pData[5] >> 4) & 0x000C) >> 2;
+    RC_CtrlData.rc.s2 = ((pData[5] >> 4) & 0x0003);
+
+    RC_CtrlData.mouse.x = ((int16_t)pData[6]) | ((int16_t)pData[7] << 8);
+    RC_CtrlData.mouse.y = ((int16_t)pData[8]) | ((int16_t)pData[9] << 8);
+    RC_CtrlData.mouse.z = ((int16_t)pData[10]) | ((int16_t)pData[11] << 8);
+
+    RC_CtrlData.mouse.press_l = pData[12];
+    RC_CtrlData.mouse.press_r = pData[13];
+
+    RC_CtrlData.key.v = ((int16_t)pData[14]); // | ((int16_t)pData[15] << 8);
+
+    // your control code ….
+}
+
+/******************************************************************************/
+
+// 此处的官方源码是UART2的中断服务程序，主要关注空闲中断，利用DMA收发数据
